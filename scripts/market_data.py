50e767982c93fa83618b206b2ca892c587fd280b
Jackson Giemza <jackson.giemza@gmail.com>
2026-06-17 13:10:31 -0600

    finished main functionality of market data script

--- scripts/market_data.py
+++ scripts/market_data.py
@@ -1,22 +1,75 @@
 import pandas as pd
 import yfinance as yf
 
-def get_price_history(portfolio_path, start_date, end_date):
+class MarketData:
+    def __init__(self, tickers, start_date, end_date):
+        self.tickers = tickers
+        self.start_date = start_date
+        self.end_date = end_date
+        self.prices = pd.DataFrame()
+        self.returns = pd.DataFrame()
 
-    portfolio = pd.read_csv(portfolio_path)
-    tickers = list(portfolio.symbol)
+    def get_price_history(self):
+        # get data from yahoo finance api using initialized variables
+        history = yf.download(self.tickers, start=self.start_date, end=self.end_date,auto_adjust=True)
 
-    history = yf.download(tickers, start=start_date, end=end_date,auto_adjust=True)
+        # populate prices df with the close data from history
+        self.prices = history['Close']
+        
+        return self.prices
 
-    adj_close_df = history['Close']
+    def save_prices_to_csv(self):
+        self.prices.to_csv(rf'../data/port_ticker_history_{self.start_date}_{self.end_date}.csv')
 
-    adj_close_df.to_csv(rf'../data/port_ticker_history_{start_date}_{end_date}.csv')
+    def validate_prices(self): 
+        # check if prices df is empty, and raise value error if it is
+        if self.prices.empty:
+            raise ValueError("Prices DataFrame is empty.")
+        
+        # get shape tuple of prices df
+        rows, columns = self.prices.shape
+        
+        # check the date index of prices for duplicates and return num of unique duplicates
+        duplicate_dates = len(self.prices.index[self.prices.index.duplicated()].unique())
+        
+        if duplicate_dates > 0:
+            raise ValueError("Duplicate dates within prices df")
 
-def save_prices_to_csv():
-    pass
+        # find missing values
+        missing_values = self.prices.isna().sum().sum()
 
-def validate_prices():
-    pass
+        date_range = (
+        self.prices.index.min(),
+        self.prices.index.max()
+        )
+        
+        info = {
+            "rows": rows,
+            "columns": columns,
+            "missing_values": missing_values,
+            "duplicate_dates": duplicate_dates,
+            "is_sorted": self.prices.index.is_monotonic_increasing,
+            "start_date": date_range[0],
+            "end_date": date_range[1]
+        }
 
-def calculate_prices():
-    pass
+        return info
+
+    def calculate_returns(self):
+        # validate data
+        self.validate_prices()
+
+        if self.prices.empty:
+            raise ValueError("Prices DataFrame is empty.")
+        
+        self.returns = (self.prices / self.prices.shift(1)) - 1
+
+        return self.returns
+    
+if __name__ == "__main__":
+    tickers = ['EEM', 'GLD', 'HYG', 'IWM', 'QQQ', 'SPY', 'TLT', 'USO', 'UUP']
+    market = MarketData(tickers=tickers, start_date='2026-05-18',end_date='2026-06-17')
+    prices = market.get_price_history()
+
+    val = market.validate_prices()
+    print(val)
