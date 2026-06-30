df38bed46263d44dd456c5762212b8484482d869
Jackson Giemza <jackson.giemza@gmail.com>
2026-06-29 23:25:24 -0600

    adding instrument data class

---
+++ src/instrument_data.py
@@ -0,0 +1,24 @@
+
+
+class InstrumentData:
+    def __init__(self):
+        pass
+    
+    def get(self):
+        pass
+    
+    def load_etfs(self):
+        pass
+
+    def load_futures(self):
+        pass
+
+    def load_fx(self):
+        pass
+
+    def load_options(self):
+        pass
+
+def main():
+    pass
+
