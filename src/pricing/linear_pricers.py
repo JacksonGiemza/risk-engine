e65e0de819b0b65a3b94798148cbc513846127b8
Jackson Giemza <jackson.giemza@gmail.com>
2026-07-04 20:03:53 -0600

    created and ignored .env

--- src/pricing/linear_pricers.py
+++ src/pricing/linear_pricers.py
@@ -3,10 +3,16 @@ from src.pricing.models import (
     Position
 )
 
-class LinearPricing:
-    def __init__(self, position: Position) -> PricingResult:
-        self.position = position
+from src.instruments.models import (
+    FutureMetadata,
+    FXMetadata,
+    OptionMetadata
+)
 
+
+class LinearPricing:
+    def __init__(self, position: Position):
+        self.position = position
 
     def etf_pricing(self):
         market_value = self.position.quantity * self.position.latest_price
@@ -17,4 +23,20 @@ class LinearPricing:
             abs_exposure=abs_exposure
         )
     
-    
+    def future_pricing(self, metadata: FutureMetadata):
+        market_value = self.position.quantity * self.position.latest_price * metadata.multiplier
+        abs_exposure = abs(market_value)
+
+        return PricingResult(
+            market_value=market_value,
+            abs_exposure=abs_exposure
+        )
+
+    def fx_pricing(self, metadata: FXMetadata):
+        quote_value = self.position.quantity * self.position.latest_price
+        
+        if metadata.quote_currency == self.position.currency:
+            market_value = quote_value
+        
+        else:
+            market_value = 
